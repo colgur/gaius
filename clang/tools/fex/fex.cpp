570b0bd3b12a38b3eb2737621e755b758be77ce7
Chad Colgur <colgur@gmail.com>
2010-11-26 12:04:26 -0800

    Check-point before cutting over to forked mirrors

--- clang/tools/fex/fex.cpp
+++ clang/tools/fex/fex.cpp
@@ -1,15 +1,33 @@
-#include "llvm/Support/CommandLine.h"
+#include "clang/Driver/Driver.h"
+#include "clang/Frontend/DiagnosticOptions.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
 
-static llvm::cl::opt<std::string>
-OutputFilename("o", llvm::cl::desc("Specify output filename"), llvm::cl::value_desc("filename"));
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/System/Host.h"
+#include "llvm/System/Path.h"
 
-static llvm::cl::opt<std::string>
-InputFilenames(llvm::cl::Positional, llvm::cl::desc("<input file>"));
+using namespace clang;
+using namespace clang::driver;
 
-int
-main (int argc, char ** argv)
-{
-  llvm::cl::ParseCommandLineOptions(argc, argv, "fex");
-  return (0);
+llvm::sys::Path GetExecutablePath(const char *Argv0) {
+  // This just needs to be some symbol in the binary; C++ doesn't
+  // allow taking the address of ::main however.
+  void *MainAddr = (void*) (intptr_t) GetExecutablePath;
+  return llvm::sys::Path::GetMainExecutable(Argv0, MainAddr);
 }
 
+int main(int argc, const char **argv, char * const *envp) {
+  //  void *MainAddr = (void*) (intptr_t) GetExecutablePath;
+  llvm::sys::Path Path = GetExecutablePath(argv[0]);
+  TextDiagnosticPrinter *DiagClient =
+    new TextDiagnosticPrinter(llvm::errs(), DiagnosticOptions());
+
+  Diagnostic Diags(DiagClient);
+  Driver TheDriver(Path.str(), llvm::sys::getHostTriple(),
+                   "a.out", /*IsProduction=*/false, /*CXXIsProduction=*/false,
+                   Diags);
+  TheDriver.setTitle("fex analyzer");
+  TheDriver.PrintHelp(true);
+
+  return( 0 );
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,5 @@
 Essentially external backing of my experiments (like most of my repos actually).
+
+Notes for clang:
+- USEDLIBS in 'tools/fex' treats clang libraries as targets
+  	   Work around this issue with a link: $(PROJ_OBJ_ROOT)/Debug+Asserts/lib -> $(LLVM_OBJ_ROOT)/Debug+Asserts/lib
